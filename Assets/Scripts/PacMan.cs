bd5d010e38a39f2e711a5d7930f36a79c29f6d62
Stephanie <stephaniiehallberg@gmail.com>
2018-08-14 01:10:49 +0200

    Part 4 - Move Node to Node [finished]

--- Assets/Scripts/PacMan.cs
+++ Assets/Scripts/PacMan.cs
@@ -7,36 +7,54 @@ public class PacMan : MonoBehaviour {
 	public float speed = 4.0f;
 	private Vector2 direction = Vector2.zero;
 
+	// Store Pac-Man current Position 
+	private Node currentNode;
+
 	// Use this for initialization
 	void Start () {
-		
+		Node node = GetNodeAtPosition (transform.localPosition);
+
+		// Find which Pellet PacMan is at 
+		if (node != null) {
+			currentNode = node;
+			Debug.Log (currentNode);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
         checkInput();
-		Move();
+		//Move();
 		UpdateOrientation();
 		
 	}
 
     void checkInput () {
-
 		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
 			direction = Vector2.left;
+			MoveToNode(direction);
 		} else if ( Input.GetKeyDown (KeyCode.RightArrow)) {
 			direction = Vector2.right;
+			MoveToNode(direction);
 		} else if ( Input.GetKeyDown (KeyCode.UpArrow)) {
 			direction = Vector2.up;
+			MoveToNode(direction);
 		} else if ( Input.GetKeyDown (KeyCode.DownArrow)) {
 			direction = Vector2.down;
+			MoveToNode(direction);
 		}
-
     }
 
 	void Move () {
 		transform.localPosition += (Vector3)(direction * speed) * Time.deltaTime;
+	}
+
+	void MoveToNode (Vector2 d) {
+		Node moveToNode = CanMove (d);
+		if (moveToNode != null) {
+			transform.localPosition = moveToNode.transform.position;
+			currentNode = moveToNode;
+		}
 	}
 
 	void UpdateOrientation () {
@@ -55,8 +73,28 @@ public class PacMan : MonoBehaviour {
 		} else if (direction == Vector2.down ) {
 			transform.localScale = new Vector3 (1, 1, 1);
 			transform.localRotation = Quaternion.Euler (0,0,270);
-
 		}
+	}
 
+	// Press a Button - Node checks Valid Positions
+	// If Ok, Pac-Man position is set to neighbor Node. 
+	Node CanMove (Vector2 d) {
+		Node moveToNode = null;
+		for (int i = 0; i < currentNode.neighbors.Length; i++)
+		{
+			if (currentNode.validDirections [i] == d) {
+				moveToNode = currentNode.neighbors [i];
+				break;
+			}
+		}
+		return moveToNode;
+	}
+
+	Node GetNodeAtPosition (Vector2 pos) {
+		GameObject tile = GameObject.Find("Game").GetComponent<GameBoard>().board [(int)pos.x, (int)pos.y];
+		if (tile != null) {
+			return tile.GetComponent<Node> ();
+		}
+		return null;
 	}
 }
